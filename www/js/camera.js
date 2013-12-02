40fd0a86d25ac3731bb6837c70a62cdb2a880e95
rspach <rspach@Himinglaeva.local>
2013-12-02 16:03:15 -0500

    camera stuff

--- www/js/camera.js
+++ www/js/camera.js
@@ -8,7 +8,9 @@ var camera = {
           document.addEventListener("deviceready", onDeviceReady, false);
      },
      onDeviceReady: function() {
-//request the persistent file system
+          pictureSource = navigator.camera.PictureSourceType;
+          destinationType = navigator.camera.DestinationType;
+          //request the persistent file system
           window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSystemSuccess, fileSystemFail);
      },
      fileSystemSuccess: function(fileSystem) {
@@ -40,20 +42,31 @@ var camera = {
      fileSystemFail: function(evt) {
           console.log(evt.target.error.code);
      },
-     getPhoto: function(source) {
-// Retrieve image file location from specified source
-          navigator.camera.getPicture(onPhotoURISuccess, onFail, {quality: 50,
-               destinationType: destinationType.FILE_URI,
-               sourceType: source});
-     },
-// Called when a photo is successfully retrieved
-//
-     onPhotoURISuccess: function(imageURI) {
-// Uncomment to view the image file URI
-// console.log(imageURI);
+     // Called when a photo is successfully retrieved
+     //
+     onPhotoDataSuccess: function(imageData) {
+          // Uncomment to view the base64-encoded image data
+          // console.log(imageData);
 
-// Get image handle
-//
+          // Get image handle
+          //
+          var smallImage = document.getElementById('smallImage');
+
+          // Unhide image elements
+          //
+          smallImage.style.display = 'block';
+
+          // Show the captured photo
+          // The inline CSS rules are used to resize the image
+          //
+          smallImage.src = "data:image/jpeg;base64," + imageData;
+     },
+     onPhotoURISuccess: function(imageURI) {
+          // Uncomment to view the image file URI
+          // console.log(imageURI);
+
+          // Get image handle
+          //
           var largeImage = document.getElementById('largeImage');
           // Unhide image elements
           //
@@ -62,7 +75,39 @@ var camera = {
           // The inline CSS rules are used to resize the image
           //
           largeImage.src = imageURI;
+     },
+     capturePhoto: function() {
+          // Take picture using device camera and retrieve image as base64-encoded string
+          navigator.camera.getPicture(this.onPhotoDataSuccess, this.onFail, {quality: 70,
+               destinationType: destinationType.DATA_URL});
+     },
+     // A button will call this function
+//
+     getPhoto: function(source) {
+          // Retrieve image file location from specified source
+          navigator.camera.getPicture(this.nPhotoURISuccess, this.onFail, {quality: 70,
+               destinationType: destinationType.FILE_URI,
+               sourceType: source});
+     },
+     capturePhotoEdit: function() {
+          // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+          navigator.camera.getPicture(onPhotoDataSuccess, onFail, {quality: 20, allowEdit: true,
+               destinationType: destinationType.DATA_URL});
+     },
+     onFail: function(message) {
+          alert('Failed because: ' + message);
      }
 
 
 };
+
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value
+
+
+
+
+
+// Called if something bad happens.
+//
+
